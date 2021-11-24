9ac673f24298768810520c8c4a4ea3783bc2758c
Pimentel <EPIMENT5@ford.com>
2021-11-24 09:19:43 -0300

    funcao doWork atualizada do BlurWorker (desfocar o URI fornecido)

--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -2,8 +2,11 @@ package com.example.background.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.text.TextUtils
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.example.background.KEY_IMAGE_URI
 import com.example.background.R
 
 class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
@@ -11,12 +14,25 @@ class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
     override fun doWork(): Result {
         val appContext = applicationContext
 
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+
         makeStatusNotification("Blurring image", appContext)
 
         return try {
-            val picture = BitmapFactory.decodeResource(
-                appContext.resources,
-                R.drawable.test)
+//            val picture = BitmapFactory.decodeResource(
+//                appContext.resources,
+//                R.drawable.test)
+
+            if (TextUtils.isEmpty(resourceUri)){
+                //Timber.e("Invalid input uri")
+                throw IllegalArgumentException("Invalid input uri")
+            }
+
+            val resolver = appContext.contentResolver
+
+            val picture = BitmapFactory.decodeStream(
+                resolver.openInputStream(Uri.parse(resourceUri))
+            )
 
             val output = blurBitmap(picture, appContext)
 
